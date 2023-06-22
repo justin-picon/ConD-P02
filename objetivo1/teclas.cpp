191bba882ff213423702a9fd310259cc1c9b8425
justin-picon <justin.picon@ucuenca.edu.ec>
2023-06-22 18:58:43 -0500

    implementa funciones de librerado y presionado

--- objetivo1/teclas.cpp
+++ objetivo1/teclas.cpp
@@ -7,6 +7,7 @@ typedef enum{
   BUTTON_DOWN
 } estadosMEF_t;
 
+int contador = 0;
 estadosMEF_t estadoActual;
 
 void InicializarMEF(void){
@@ -16,4 +17,17 @@ void InicializarMEF(void){
   estadoActual = BUTTON_UP;
 
   Serial.begin(9600);
+}
+
+void buttonPressed(int16_t tecla){
+  Serial.print("Se ha presionado la tecla del pin: ");
+  Serial.println(tecla);
+}
+
+void buttonReleased(int16_t tecla){
+  contador++;
+  Serial.print("Se ha liberado la tecla del pin: ");
+  Serial.println(tecla);
+  Serial.print("Contador de pulsaciones: ");
+  Serial.println(contador);
 }
