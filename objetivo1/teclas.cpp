875f5c6321a46b37f0225b35224cabbfa5838939
justin-picon <justin.picon@ucuenca.edu.ec>
2023-06-22 18:52:22 -0500

    Se añade la función de inicialización

--- objetivo1/teclas.cpp
+++ objetivo1/teclas.cpp
@@ -6,3 +6,14 @@ typedef enum{
   BUTTON_RISING,
   BUTTON_DOWN
 } estadosMEF_t;
+
+estadosMEF_t estadoActual;
+
+void InicializarMEF(void){
+  pinMode(SW1, INPUT);
+  pinMode(SW2, INPUT);
+
+  estadoActual = BUTTON_UP;
+
+  Serial.begin(9600);
+}
