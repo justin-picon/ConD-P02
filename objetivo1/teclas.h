5aff0f5efb44c46958e1bd526f8c7083e877c590
justin-picon <justin.picon@ucuenca.edu.ec>
2023-06-22 18:47:51 -0500

    Agregamos cabecera a teclas.h

--- objetivo1/teclas.h
+++ objetivo1/teclas.h
@@ -1,1 +1,13 @@
 #include <Arduino.h>
+
+#define SW1 3
+#define SW2 8
+#define RETARDO 40
+
+void InicializarMEF(void);
+void ActualizarMEF(void);
+
+void buttonPressed(int16_t tecla);
+void buttonReleased(int16_t tecla);
+
+bool espera(unsigned long intervalo);
