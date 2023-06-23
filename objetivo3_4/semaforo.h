27cac6d19de65a6a486e65ca166a0278fcc49c77
Skyflash2305 <marlonsk1@hotmail.com>
2023-06-22 20:40:25 -0500

    Funciones del semáforo

---
+++ objetivo3_4/semaforo.h
@@ -0,0 +1,5 @@
+#include <Arduino.h>
+int inicializarMEF(int led_Rojo, int led_Amarillo, int led_Verde);
+int actualizarMEF(int pinPulsador, int pinPulsador1);
+int modo1(void);
+int est_tecla(void);
