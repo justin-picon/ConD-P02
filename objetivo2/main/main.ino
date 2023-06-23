862d1142f06f8ccc736655fc79d587437ea40c59
justin-picon <justin.picon@ucuenca.edu.ec>
2023-06-22 19:14:53 -0500

    Llama a las funciones en el main.ino

--- objetivo2/main/main.ino
+++ objetivo2/main/main.ino
@@ -1,5 +1,14 @@
 #include "main.h"
 
-void setup(){}
+dbn_t dataTecla1 = {SW1, RETARDO_SW1, BUTTON_UP};
+dbn_t dataTecla2 = {SW2, RETARDO_SW2, BUTTON_UP};
 
-void loop(){}
+void setup(){
+  InicializarMEF(&dataTecla1);
+  InicializarMEF(&dataTecla2);
+}
+
+void loop(){
+  ActualizarMEF(&dataTecla1);
+  ActualizarMEF(&dataTecla2);
+}
